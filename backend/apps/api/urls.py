31fe5c4be1dbfd04e0106bb9db8302b6c35fcf68
vladpaun1 <vlad.paun06@gmail.com>
2025-10-04 14:32:24 +0200

    exposed upload api endpoint

--- backend/apps/api/urls.py
+++ backend/apps/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-
-from .views import PingView
+from .views import PingView, AnalyzeView
 
 urlpatterns = [
     path("ping/", PingView.as_view(), name="ping"),
+    path("analyze/", AnalyzeView.as_view(), name="analyze"),
 ]
