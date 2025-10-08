b517f7b05c50a0fa4a9d5d9c9087a8f5b6deece2
N1wan <niwan.b.v.h@gmail.com>
2025-10-09 00:13:12 +0200

    cleanup

--- backend/services/asr/__init__.py
+++ backend/services/asr/__init__.py
@@ -1,5 +1,4 @@
 from .vosk_engine import VoskASREngine
 from .asr_whisper import WhisperASREngine
 
-__all__ = ["VoskASREngine"]
-__all__ = ["WhisperASREngine"]
+__all__ = ["VoskASREngine", "WhisperASREngine"]
