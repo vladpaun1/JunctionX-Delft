6bffa4d5351d633883e6fa06158bed687d28f20d
vladpaun1 <vlad.paun06@gmail.com>
2025-10-11 02:24:54 +0200

    set vite as proxy for django api calls

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,20 @@
+/// <reference types="node" />
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: true,
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://web:8000',
+        changeOrigin: true,
+        rewrite: (p) => p.replace(/^\/api/, ''),
+      },
+    },
+  },
   plugins: [react()],
 })
